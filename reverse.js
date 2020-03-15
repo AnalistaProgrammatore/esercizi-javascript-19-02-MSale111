726585857a169b34096e5582d73da1044cecb550
MSale111 <59830912+MSale111@users.noreply.github.com>
2020-03-15 12:50:44 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -3,7 +3,17 @@
 * Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
 **/
 let array = [1,2,3,4,5,6];
+
 let arrayR = [...array].reverse();
 
+function reverseImperativo (array){
+    let arrayI= [];
+    for (let i=array.length -1 ; i >= 0 ; i--) {
+    arrayI.push(array[i]);
+    }
+return arrayI;
+}
+
 console.log(array);
+console.log(reverseImperativo (array));
 console.log(arrayR);
