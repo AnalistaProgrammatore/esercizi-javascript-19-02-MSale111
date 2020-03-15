d4b12f0bcf40f903652b09122dcbc099a703fcac
MSale111 <59830912+MSale111@users.noreply.github.com>
2020-03-15 12:36:51 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -2,3 +2,8 @@
 * Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
 * Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
 **/
+let array = [1,2,3,4,5,6];
+let arrayR = [...array].reverse();
+
+console.log(array);
+console.log(arrayR);
